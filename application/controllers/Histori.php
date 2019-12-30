74aefa80cdbe95f97ace506875ff86ae0007e532
kotokareh@gmail.com <kotokareh@gmail.com>
2019-12-30 12:54:03 +0700

    yes perbaikan

--- application/controllers/Histori.php
+++ application/controllers/Histori.php
@@ -18,34 +18,34 @@ class Histori extends CI_Controller
   public function index()
   {
     $x['judul'] = 'Log akses system.';
-   $this->template->load('template','histori/histori_list',$x);
- } 
+    $this->template->load('template','histori/histori_list',$x);
+  } 
 
- public function json() {
-  header('Content-Type: application/json');
-  echo $this->Histori_model->json();
-}
-
-public function detail($id) 
-{
-  $row = $this->Histori_model->get_by_id($id);
-  if ($row) {
-    $data = array(
-      'id_histori' => $row->id_histori,
-      'id_user' => $row->id_user,
-      'url' => $row->url,
-      'aktivitasi' => $row->aktivitasi,
-      'tanggal' => $row->tanggal,
-      'ip_address' => $row->ip_address,
-      'browser' => $row->browser, 
-      'judul'=>'Histori akses log',
-    );
-    $this->template->load('template','histori/histori_read', $data);
-  } else {
-    $this->session->set_flashdata('message', '<div class="alert alert-warniing fade-in">Data Tidak Di Temukan.</div>');
-    redirect(site_url('histori'));
+  public function json() {
+    header('Content-Type: application/json');
+    echo $this->Histori_model->json();
   }
-}
+
+  public function detail($id) 
+  {
+    $row = $this->Histori_model->get_by_id($id);
+    if ($row) {
+      $data = array(
+        'id_histori' => $row->id_histori,
+        'id_user' => $row->id_user,
+        'url' => $row->url,
+        'aktivitasi' => $row->aktivitasi,
+        'tanggal' => $row->tanggal,
+        'ip_address' => $row->ip_address,
+        'browser' => $row->browser, 
+        'judul'=>'Histori akses log',
+      );
+      $this->template->load('template','histori/histori_read', $data);
+    } else {
+      $this->session->set_flashdata('message', '<div class="alert alert-warniing fade-in">Data Tidak Di Temukan.</div>');
+      redirect(site_url('histori'));
+    }
+  }
 
 
 }
